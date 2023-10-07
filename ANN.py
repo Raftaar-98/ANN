7cbd834931c12ad1fdc92bfdd40d4a0ad74ffe80
Shishir Yalburgi <shishiryalburgi@gmail.com>
2023-10-07 17:32:00 -0500

    2nd useful commit branch from now on
    please create and work on your branch

---
+++ ANN.py
@@ -0,0 +1,9 @@
+#
+#
+# Author 1 : Shishir Sunil Yalburgi     NETID: SSY220000
+#
+# Author 2 : Uthama Kadengodlu          NETID: UXK210012
+# Implements Artificial Neural Network
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
